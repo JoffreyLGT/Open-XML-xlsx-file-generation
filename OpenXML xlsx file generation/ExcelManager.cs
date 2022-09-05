721e38b1961a1e1715fdf6e69a43f4180ef05071
Joffrey LAGUT <j.lagut@live.fr>
2022-09-05 16:20:16 +0200

    feat: version using a class to write information line by line

---
+++ OpenXML xlsx file generation/ExcelManager.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace OpenXML_xlsx_file_generation
+{
+    internal class ExcelManager:IDisposable
+    {
+        private SpreadsheetDocument spreadsheetDocument;
+        private WorkbookPart workbookpart;
+        private WorksheetPart worksheetPart;
+
+        public ExcelManager() { }
+
+        /// <summary>
+        /// Create a new instance of ExcelManager and create a new file.
+        /// </summary>
+        /// <param name="fileFullPath">full path of the file (folder + file name + extension)</param>
+        /// <param name="sheetName">name of the sheet to add, null not to add any</param>
+        /// <remarks>use CreateSheet to create a new sheet if you don't provide sheetName</remarks>
+        public ExcelManager(string fileFullPath, string sheetName = null)
+        {
+            CreateFile(fileFullPath, sheetName);
+        }
+
+        /// <summary>
+        /// Create a new instance of ExcelManager and create a new file.
+        /// </summary>
+        /// <param name="folder">folder that will receive the file</param>
+        /// <param name="fileName">name of the file including its extension</param>
+        /// <param name="sheetName">name of the sheet to add, null not to add any</param>
+        public ExcelManager(string folder, string fileName, string sheetName = null)
+        {
+            CreateFile(Path.Combine(folder, fileName), sheetName);
+        }
+
+        public void Dispose()
+        {
+            Close();
+            spreadsheetDocument.Dispose();
+        }
+
+
+
+        /// <summary>
+        /// Create a new file.
+        /// </summary>
+        /// <param name="path">full path to the file to create</param>
+        /// <param name="sheetName">name of the sheet to add, null not to add any</param>
+        /// <remarks>use CreateSheet to create a new sheet if you don't provide sheetName</remarks>
+        public void CreateFile(string path, string sheetName = null)
+        {
+            spreadsheetDocument = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook);
+            workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            if (!string.IsNullOrEmpty(sheetName))
+            {
+                CreateSheet(sheetName);
+            }
+        }
+
+        /// <summary>
+        /// Create a new sheet in the currently opened Workbook.
+        /// </summary>
+        /// <param name="name">name of the sheet</param>
+        public void CreateSheet(string name)
+        {
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild(new Sheets());
+
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = name
+            };
+            sheets.Append(sheet);
+            spreadsheetDocument.Save();
+        }
+
+        /// <summary>
+        /// Write the line content in the file.
+        /// </summary>
+        /// <param name="lineNumber">number of the line to write</param>
+        /// <param name="lineContent">content of the line</param>
+        public void WriteLine(int lineNumber, string[] lineContent, bool save = true)
+        {
+            for (int i = 0; i < lineContent.Count(); i++)
+            {
+                Cell cell = InsertCellInWorksheet(GetExcelColumnName(i + 1), (uint)lineNumber, worksheetPart);
+
+                cell.CellValue = new CellValue(lineContent[i]);
+                cell.DataType = new EnumValue<CellValues>(CellValues.String);
+            }
+            if (save)
+            {
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Save the Excel file.
+        /// </summary>
+        public void Save()
+        {
+            worksheetPart.Worksheet.Save();
+        }
+
+        /// <summary>
+        /// Create a new Excel file and insert the values in a worksheet.
+        /// </summary>
+        /// <param name="docName"></param>
+        /// <param name="allValues"></param>
+        public static void CreateNewFileWithValues(string fileName, string worksheetName, List<string[]> allValues)
+        {
+            using ExcelManager manager = new ExcelManager(fileName, worksheetName);
+                for (int j = 0; j < allValues.Count; j++)
+                {
+                    manager.WriteLine(j + 1, allValues[j], false);
+                }
+            manager.Save();
+        }
+
+        private void Close()
+        {
+            spreadsheetDocument.Close();
+        }
+
+        /// <summary>
+        /// Insert a cell in the worksheet.
+        /// If it already exists, return it.
+        /// </summary>
+        /// <param name="columnName"></param>
+        /// <param name="rowIndex"></param>
+        /// <param name="worksheetPart"></param>
+        /// <returns>The cell matching {columnName:rowIndex}</returns>
+        /// <remarks>Function found on https://docs.microsoft.com/en-us/office/open-xml/how-to-insert-text-into-a-cell-in-a-spreadsheet?source=recommendations and edited a bit to check parameters.</remarks>
+        private Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            if (string.IsNullOrEmpty(columnName))
+            {
+                throw new ArgumentNullException(nameof(columnName) + " cannot be empty.");
+            }
+            if (rowIndex == 0)
+            {
+                throw new ArgumentNullException(nameof(rowIndex) + " must be bigger than 0.");
+            }
+
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+
+            // If the worksheet does not contain a row with the specified row index, insert one.
+            Row row;
+            if (sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
+            {
+                row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+
+            // If there is not a cell with the specified column name, insert one.  
+            if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
+            {
+                return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+            }
+            else
+            {
+                // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+                Cell refCell = null;
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+
+                Cell newCell = new Cell() { CellReference = cellReference };
+                row.InsertBefore(newCell, refCell);
+
+                worksheet.Save();
+                return newCell;
+            }
+        }
+
+        /// <summary>
+        /// Get the Excel column name from the column number.
+        /// Example: 1 returns A, 2 returns B etc.
+        /// </summary>
+        /// <param name="columnNumber"></param>
+        /// <returns></returns>
+        /// <remarks>Function found on https://stackoverflow.com/a/182924</remarks>
+        private string GetExcelColumnName(int columnNumber)
+        {
+            string columnName = "";
+            while (columnNumber > 0)
+            {
+                int modulo = (columnNumber - 1) % 26;
+                columnName = Convert.ToChar('A' + modulo) + columnName;
+                columnNumber = (columnNumber - modulo) / 26;
+            }
+            return columnName;
+        }
+
+    }
+}
--- OpenXML xlsx file generation/Program.cs
+++ OpenXML xlsx file generation/Program.cs
@@ -25,145 +25,20 @@ namespace OpenXML_xlsx_file_generation
                 new string[] { "2", "Dupond" }
             };
             // Information regarding the file to create
-            string fileName = "DataFile.xlsx";
-            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+            string fileName = "DataFile-Automatic.xlsx";
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string sheetName = "Data";
-            // Call the function to create the file and insert the values inside
-            CreateNewFileWithValues(filePath, sheetName,  rows);
+
+            // Use the static method to create and write content in the file
+            ExcelManager.CreateNewFileWithValues(Path.Combine(folder, fileName), sheetName, rows);
+
+            // Create a new instance of ExcelManager and write specific lines
+            fileName = "DataFile-Manual.xlsx";
+            using ExcelManager excel = new ExcelManager(folder, fileName, sheetName);
+            // Write content only in line 1, 3 and 5
+            excel.WriteLine(1, rows[0]);
+            excel.WriteLine(3, rows[1]);
+            excel.WriteLine(5, rows[2]);
         }
-
-
-        /// <summary>
-        /// Create a new Excel file and insert the values in a worksheet.
-        /// </summary>
-        /// <param name="docName"></param>
-        /// <param name="allValues"></param>
-        public static void CreateNewFileWithValues(string fileName, string worksheetName, List<string[]> allValues)
-        {
-            // Create a new file
-            using SpreadsheetDocument spreadSheet = SpreadsheetDocument.
-                Create(fileName, SpreadsheetDocumentType.Workbook);
-            {
-                // Add a Workbook to the document
-                WorkbookPart workbookpart = spreadSheet.AddWorkbookPart();
-                workbookpart.Workbook = new Workbook();
-
-                // Add a WorksheetPart to the WorkbookPart.
-                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
-                worksheetPart.Worksheet = new Worksheet(new SheetData());
-
-                // Add Sheets to the Workbook.
-                Sheets sheets = spreadSheet.WorkbookPart.Workbook.
-                    AppendChild(new Sheets());
-
-                // Append a new worksheet and associate it with the workbook.
-                Sheet sheet = new Sheet()
-                {
-                    Id = spreadSheet.WorkbookPart.
-                    GetIdOfPart(worksheetPart),
-                    SheetId = 1,
-                    Name = worksheetName
-                };
-                sheets.Append(sheet);
-
-                for (int j = 0; j < allValues.Count; j++)
-                {
-                    var rowText = allValues[j];
-                    for (int i = 0; i < rowText.Count(); i++)
-                    {
-                        // Insert cell A1 into the new worksheet.
-                        Cell cell = InsertCellInWorksheet(GetExcelColumnName(i + 1), (uint) (j + 1), worksheetPart);
-
-                        // Set the value of cell A1.
-                        cell.CellValue = new CellValue(rowText[i]);
-                        cell.DataType = new EnumValue<CellValues>(CellValues.String);
-                    }
-                }
-                // Save the new worksheet.
-                worksheetPart.Worksheet.Save();
-            }
-        }
-
-        /// <summary>
-        /// Insert a cell in the worksheet.
-        /// If it already exists, return it.
-        /// </summary>
-        /// <param name="columnName"></param>
-        /// <param name="rowIndex"></param>
-        /// <param name="worksheetPart"></param>
-        /// <returns>The cell matching {columnName:rowIndex}</returns>
-        /// <remarks>Function found on https://docs.microsoft.com/en-us/office/open-xml/how-to-insert-text-into-a-cell-in-a-spreadsheet?source=recommendations and edited a bit to check parameters.</remarks>
-        private static Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
-        {
-            if (string.IsNullOrEmpty(columnName))
-            {
-                throw new ArgumentNullException(nameof(columnName) + " cannot be empty.");
-            }
-            if (rowIndex == 0)
-            {
-                throw new ArgumentNullException(nameof(rowIndex) + " must be bigger than 0.");
-            }
-
-            Worksheet worksheet = worksheetPart.Worksheet;
-            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
-            string cellReference = columnName + rowIndex;
-
-            // If the worksheet does not contain a row with the specified row index, insert one.
-            Row row;
-            if (sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
-            {
-                row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
-            }
-            else
-            {
-                row = new Row() { RowIndex = rowIndex };
-                sheetData.Append(row);
-            }
-
-            // If there is not a cell with the specified column name, insert one.  
-            if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
-            {
-                return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
-            }
-            else
-            {
-                // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
-                Cell refCell = null;
-                foreach (Cell cell in row.Elements<Cell>())
-                {
-                    if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
-                    {
-                        refCell = cell;
-                        break;
-                    }
-                }
-
-                Cell newCell = new Cell() { CellReference = cellReference };
-                row.InsertBefore(newCell, refCell);
-
-                worksheet.Save();
-                return newCell;
-            }
-        }
-
-        /// <summary>
-        /// Get the Excel column name from the column number.
-        /// Example: 1 returns A, 2 returns B etc.
-        /// </summary>
-        /// <param name="columnNumber"></param>
-        /// <returns></returns>
-        /// <remarks>Function found on https://stackoverflow.com/a/182924</remarks>
-        private static string GetExcelColumnName(int columnNumber)
-        {
-            string columnName = "";
-            while (columnNumber > 0)
-            {
-                int modulo = (columnNumber - 1) % 26;
-                columnName = Convert.ToChar('A' + modulo) + columnName;
-                columnNumber = (columnNumber - modulo) / 26;
-            }
-            return columnName;
-        }
-
     }
 }
